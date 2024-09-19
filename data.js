5f85b446e078dbb3bcef37919e0939ad32a576cd
Yab112 <Yab112@users.noreply.github.com>
2024-09-19 09:07:08 +0000

    deploy: e15785b915c0bfccc9a39a4e80cf0f58aae2877d

---
+++ data.js
@@ -0,0 +1,62 @@
+export const projectsData = {
+    "projects": [
+      {
+        "title": "Simple Log In",
+        "description": "A simple log in page with a login form and a sign up option.",
+        "techstack": ["html", "CSS", "bootstrap"],
+        "image": "login.png",
+        "link":"https://yab112.github.io/Dot/day-5/index.html"
+      }
+      ,{
+        "title": "Simple Pricing Page",
+        "description": "A simple Pricing  page with multiple options.",
+        "techstack": ["html", "CSS", "bootstrap"],
+        "image": "login.png",
+        "link":"https://yab112.github.io/Dot/day-6/index.html"
+      }
+      ,{
+        "title": "Simple register Page",
+        "description": "A simple register  page with for  registering a new user to the website.",
+        "techstack": ["html", "CSS", "bootstrap"],
+        "image": "register.png",
+        "link":"https://yab112.github.io/Dot/day-7/index.html"
+      }
+      ,{
+        "title": "Simple grid template",
+        "description": "A simple grid  template for  a website.",
+        "techstack": ["html", "CSS", "bootstrap"],
+        "image": "grid.png",
+        "link":"https://yab112.github.io/Dot/lab-10/index.html"
+      }
+      ,{
+        "title": "Simple form template",
+        "description": "A simple form  template for  a website.",
+        "techstack": ["html", "CSS", "bootstrap"],
+        "image": "form.png",
+        "link":"https://yab112.github.io/Dot/lab-5/index.html"
+      }
+      ,{
+        "title": "rick portfolio",
+        "description": "A simple portfolio fot rick ",
+        "techstack": ["html", "CSS", "bootstrap"],
+        "image": "form.png",
+        "link":"https://yab112.github.io/Dot/lab-13/index.html"
+      }
+      ,{
+        "title": "stanning hero section",
+        "description": "a hero section for webssite",
+        "techstack": ["html", "CSS", "bootstrap"],
+        "image": "form.png",
+        "link":"https://yab112.github.io/Dot/lb-12-assignment/index.html"
+      }
+      ,{
+        "title": "vedeo background hero section",
+        "description": "a hero section for webssite",
+        "techstack": ["html", "CSS"],
+        "image": "videoback.png",
+        "link":"https://yab112.github.io/Dot/lab-12-practice/videoBack.html"
+      }
+
+    ]
+  }
+  
